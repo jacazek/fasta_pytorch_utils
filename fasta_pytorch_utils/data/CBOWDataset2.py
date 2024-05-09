127f4ac03beeb87428ca965796780198ec0aee34
jake <dukob86@yahoo.com>
2024-05-08 23:11:54 -0400

    feat: work with array of files

--- fasta_pytorch_utils/data/CBOWDataset2.py
+++ fasta_pytorch_utils/data/CBOWDataset2.py
@@ -5,12 +5,14 @@ from fasta_utils import FastaFileReader
 
 
 class CBOWDataset2(torch.utils.data.IterableDataset):
-    def __init__(self, sequence_queue, tokenizer, vocabulary, device="cpu", dtype=torch.float32, window_size=7, rank=0):
+    def __init__(self, fasta_files, tokenizer, vocabulary, device="cpu", dtype=torch.float32, window_size=7, rank=0,
+                 device_count=1):
         super(CBOWDataset2).__init__()
         self.rank = rank
+        self.device_count = device_count
         self.tokenizer = tokenizer
         self.vocabulary = vocabulary
-        self.sequence_queue = sequence_queue
+        self.fasta_files = fasta_files
         self.device = device
         self.dtype = dtype
         self.window_size = window_size
@@ -18,25 +20,84 @@ class CBOWDataset2(torch.utils.data.IterableDataset):
 
     def __iter__(self):
         # worker_info = torch.utils.data.get_worker_info()
-        sequence = self.sequence_queue.get(True)
-        while sequence is not None:
-            iterator = iter(self.tokenizer.tokenize(sequence))
-            window = [self.vocabulary[next(iterator)] for _ in range(self.window_size)]
-            try:
-                while True:
-                    yield (torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
-                                        dtype=torch.long),
-                           torch.tensor(window[self.window_middle], dtype=torch.long))
+        for i in range(int(len(self.fasta_files) / self.device_count)):
+            fasta_file = self.fasta_files[self.rank * self.device_count + i]
+            if fasta_file is not None:
+                worker_info = torch.utils.data.get_worker_info()
 
-                    # Slide the window by one element
-                    window.pop(0)
-                    window.append(self.vocabulary[next(iterator)])
+                with FastaFileReader(fasta_file) as fasta_file_reader:
+                    if worker_info is None:
+                        data_reader = fasta_file_reader.read_all()
+                        for header, sequence in data_reader:
+                            iterator = iter(self.tokenizer.tokenize(sequence))
+                            window = [self.vocabulary[next(iterator)] for _ in range(self.window_size)]
+                            try:
+                                while True:
+                                    yield (torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
+                                                        dtype=torch.long),
+                                           torch.tensor(window[self.window_middle], dtype=torch.long))
 
-            except StopIteration:
-                # Handle the end of the streaming data
-                while len(window) == self.window_size:
-                    yield (torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
-                                        dtype=torch.long), torch.tensor(window[self.window_middle],
-                                                                        dtype=torch.long))  # Return the remaining window
-                    window.pop(0)
-            sequence = self.sequence_queue.get(True)
+                                    # Slide the window by one element
+                                    window.pop(0)
+                                    window.append(self.vocabulary[next(iterator)])
+
+                            except StopIteration:
+                                # Handle the end of the streaming data
+                                while len(window) == self.window_size:
+                                    yield (torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
+                                                        dtype=torch.long), torch.tensor(window[self.window_middle],
+                                                                                        dtype=torch.long))  # Return the remaining window
+                                    window.pop(0)
+
+                    else:
+                        number_of_sequences = fasta_file_reader.get_index_table_length()
+                        for sequence_number in range(int(number_of_sequences / worker_info.num_workers)):
+                            if sequence_number < number_of_sequences:
+                                # index = self.rank * worker_info.num_workers + worker_info.id % number_of_sequences
+                                data_reader = fasta_file_reader.read_at_index(sequence_number)
+
+                                for header, sequence in data_reader:
+                                    iterator = iter(self.tokenizer.tokenize(sequence))
+                                    window = [self.vocabulary[next(iterator)] for _ in range(self.window_size)]
+                                    try:
+                                        while True:
+                                            yield (
+                                            torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
+                                                         dtype=torch.long),
+                                            torch.tensor(window[self.window_middle], dtype=torch.long))
+
+                                            # Slide the window by one element
+                                            window.pop(0)
+                                            window.append(self.vocabulary[next(iterator)])
+
+                                    except StopIteration:
+                                        # Handle the end of the streaming data
+                                        while len(window) == self.window_size:
+                                            yield (
+                                            torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
+                                                         dtype=torch.long), torch.tensor(window[self.window_middle],
+                                                                                         dtype=torch.long))  # Return the remaining window
+                                            window.pop(0)
+
+        # sequence = self.sequence_queue.get(True)
+        # while sequence is not None:
+        #     iterator = iter(self.tokenizer.tokenize(sequence))
+        #     window = [self.vocabulary[next(iterator)] for _ in range(self.window_size)]
+        #     try:
+        #         while True:
+        #             yield (torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
+        #                                 dtype=torch.long),
+        #                    torch.tensor(window[self.window_middle], dtype=torch.long))
+        #
+        #             # Slide the window by one element
+        #             window.pop(0)
+        #             window.append(self.vocabulary[next(iterator)])
+        #
+        #     except StopIteration:
+        #         # Handle the end of the streaming data
+        #         while len(window) == self.window_size:
+        #             yield (torch.tensor(window[:self.window_middle] + window[self.window_middle + 1:],
+        #                                 dtype=torch.long), torch.tensor(window[self.window_middle],
+        #                                                                 dtype=torch.long))  # Return the remaining window
+        #             window.pop(0)
+        #     sequence = self.sequence_queue.get(True)
